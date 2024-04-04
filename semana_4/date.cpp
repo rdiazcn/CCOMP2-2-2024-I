383b2489201ff088538b78712ae155b1b8ad073f
rdiazcn <rodrigo.diaz.cucalon@ucsp.edu.pe>
2024-04-03 20:35:58 -0500

    Final

--- semana_4/date.cpp
+++ semana_4/date.cpp
@@ -3,22 +3,13 @@
 
 using namespace std;
 
-int main(){
-    Date datei (3,1,2020);
-    Date dateii (15,14,2019);
-    Date dateiii (20,12,1500);
+int main() {
+    Date date1 (3,1,2020);
+    Date date2 (15,14,2019);
+    Date date3 (20,12,1500);
 
-    datei.isLeapYear();
+    date1.displayDate();
+    date2.displayDate();
+    date3.displayDate();
 
-    datei.isDayTrue();
-
-/*) Modificar los m´etodos de la clase para validar si el a˜no esta en dentro del rango
-1900 hasta 2050, caso contrario, se debe colocar 1900 por defecto.
-(b) Agregar la funci´on bool isLeapYear() const a la clase y verificar si en la fecha
-representa un a˜no bisiesto.
-Ciencia de la Computaci´on IPractica Calificada de Laboratorio III - P´agina 3 de 3 3 de abril de 2024
-(c) Modificar los m´etodos de la clase para validar si el dia del mes es correcto. Considere
-que un a˜no bisiesto tiene 29 dias en el mes de febrero.
-(d) En la funci´on principal crear 3 objetos de tipo Date: (i) 03 / 01 / 2020; (ii) 15 / 14
-/ 2019; y (iii) 20 / 12 / 1500.
-(e) Invocar a la funci´on displayDate() para mostrar las fechas.*/
+}
--- semana_4/date.h
+++ semana_4/date.h
@@ -7,20 +7,44 @@ class Date {
     public:
         explicit Date(int d, int m, int y):
         day{d}, month{m}, year{y} {
-            if(y>= 1900 && y <=2050) {
-                    year = y;
-            }
-            if(m >= 1 && m <= 12) {
+
+            if(y>= 1900 && y <=2050) 
+                year = y;
+            else
+                year = 1900;
+
+            if(m >= 1 && m <= 12) 
                 month = m;
-            }
+            else
+                month = 1;
+
+            switch(month) {
+                case 2:
+                    if (d>=1){
+                        if (isLeapYear() && d==29)
+                            day = d;
+                        else if (!isLeapYear() && d <=28)
+                            day = d;
+                    }
+                    break;
+
+                case 4: case 6: case 9: case 11:
+                    if (d > 30)
+                        day=1;
+                    break;
+
+                default:
+                    if (d > 31)
+                        day=1;
+                    break;
+            }   
         }
+
         bool isLeapYear() const{
-            if (!year%4 && year%100){
-                cout<<"Divisble entre 4 y no entre 100";
+            if (year%4==0 && year%100!=0){
                 return true;
             }
-            else if (!year%100 && !year%400){
-                cout<<"Divisible entre 100 y 400";
+            else if (year%100==0 && year%400==0){
                 return true;
             }
 
@@ -28,34 +52,60 @@ class Date {
                 return false;
             }
                 
-        }   
+        }
+
         int getDay() const{
             return day;
         }
+        int getMonth() const{
+            return month;
+        }
+        int getYear() const{
+            return year;
+        }
 
-        void isDayTrue(){
-            switch(month){
-                case 4 || 6 || 9|| 11:
-                    if (day > 31){
-                        cout<<"El mes "<<month<<" no tiene mas de 31 dias"<<endl;
-                        day=1;
+        void setDay(int d){
+            switch(month) {
+                case 2:
+                    if (d>=1){
+                        if (isLeapYear() && d==29)
+                            day = d;
+                        else if (!isLeapYear() && d <=28)
+                            day = d;
                     }
                     break;
-                case 2:
-                    if (!isLeapYear() and day>29)
-                        cout<<"Febrero no tiene mas de 28 dias en años no bisiestos"<<endl;
-                        day = 1;
+
+                case 4: case 6: case 9: case 11:
+                    if (d > 30)
+                        day=1;
+                    break;
+
+                default:
+                    if (d > 31)
+                        day=1;
+                    break;
+
+            }
+        }
+
+        void setMonth(int m){
+            if(m >= 1 && m <= 12) {
+                month = m;
+            }
+        }
+
+        void setYear(int y){
+            if(y>= 1900 && y <=2050) 
+                year = y;
         }
 
         void displayDate(){
-            cout<<"Fecha: "<<day<<"/"<<month<<"/"<<year;
+            cout<<"Fecha: "<<day<<"/"<<month<<"/"<<year<<endl;
         }
-    }
     private:
-        int day;
+        int day{1};
         int month{1};
         int year{1900};
-
 
 
 };
@@ -63,23 +113,3 @@ class Date {
 son bisiestos. Los años que son divisibles entre 100, pero no entre 400, no son bisiestos. 
 Sin embargo, los años divisibles entre 100 y entre 400 sí que son bisiestos.*/
 
-#include <iostream>
-2
-3 class Date {
-4 public:
-5 explicit Date(int d, int m, int y) : day{d}, year{y} {
-if(m >= 1 && m <= 12) {
- month = m;
-}
-9 }
-10 void setDay(int d) {
-11 day = d;
-12 }
-13 void setMonth(int m) {
-14 if(m >= 1 && m <= 12) {
-15 month = m;
-16 }
-17 }
-18 void setYear(int y) {
-19 year = y;
-20 }
