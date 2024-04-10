cd0b5a43ac30c471670120a6fc9f9a8c5f2d8b9d
ALUMNO-P103 <ALUMNO-P103@UCSP.AULAB>
2024-04-10 08:29:21 -0500

    Ejercicios semana 5

---
+++ semana_5/fibo.cpp
@@ -0,0 +1,9 @@
+// Programa que recibe un numero y consecuentemente realiza la secuencia fibonacci de "numero"-largo
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
---
+++ semana_5/primonoprimo.cpp
@@ -0,0 +1,9 @@
+// Programa que recibe un numero y determina si es primo o no
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
