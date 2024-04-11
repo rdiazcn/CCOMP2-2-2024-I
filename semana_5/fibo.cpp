9229b6a82b05466090357d2d589ad30532967db9
rdiazcn <rodrigo.diaz.cucalon@ucsp.edu.pe>
2024-04-10 23:10:51 -0500

    Fibo y Primo

--- semana_5/fibo.cpp
+++ semana_5/fibo.cpp
@@ -5,5 +5,25 @@
 using namespace std;
 
 int main(){
+
+    int a{0}, b{1}, aux{0}; 
+    int num{0}, sum {0};
+
+    cout<<"Ingrese un numero natural: ";
+    cin>>num;
+
+    cout<<"Secuencia Fibonacci: ";
+
+    for (int i = 0; i < num; i++)
+    {   
+        cout<<a<<" ";
+        sum += a;
+
+        aux = a+b;
+        a = b;
+        b = aux;
+    }    
+    
+    cout <<"\nSuma de Secuencia: "<< sum;
     return 0;
 }
--- semana_5/primonoprimo.cpp
+++ semana_5/primonoprimo.cpp
@@ -4,6 +4,39 @@
 
 using namespace std;
 
+bool isPrimo(int n){
+
+    for(int i=2; i<=n; i++){
+        if (n%i==0 && n!=i){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
+    
+    // Manualmente
+    //int num{0};
+    //cout<<"Ingrese un numero natural: ";
+    //cin>>num;
+
+    //Valores del 1 al 100
+    for (int i=2; i<=100; i++){
+        if (isPrimo(i))
+            cout<<"El numero "<<i<<" es primo."<<endl;
+        
+        else
+            cout<<"El numero "<<i<<" es compuesto."<<endl;
+
+    }
+    
+    // Muestra los primos del 1 al 100 en una fila.
+    // for (int i=2; i<=100; i++){
+    //      if (isPrimo(i))
+    //         cout<<i<<" ";
+    // } 
+
     return 0;
 }
